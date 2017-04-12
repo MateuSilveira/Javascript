25c67317137cdb9b33d55611ca8611b943f51a6a
MateuSilveira <mateusilveira30@gmail.com>
2017-04-12 17:36:43 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function evento_entrar(){
+         alert("Seja Bem Vindo");
+         }
+function evento_sair(){
+         alert("Até a Próxima Visita");
+         }
+function evento_clique(){
+         window.document. getElementById('ID2').style.color='red';
+         }
